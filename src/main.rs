6cd129917435c6dd6d90e412d395a1aa20d27c25
lmnnarciso <lmnnarciso14@gmail.com>
2023-04-19 04:48:53 +0800

    read file start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // let data = vec![1, 2, 3];
+    // let mut foo = data.iter().map(|f| {
+    //     return f + 1;
+    // });
+
+    // let mut new_vector = vec![];
+
+    // while let Some(x) = foo.next() {
+    //     new_vector.push(x);
+    // }
+
+    // println!("my map {:?}", new_vector);
 }

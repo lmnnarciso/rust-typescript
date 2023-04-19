08dbb622c9a2b37fbd4148f4c459a151932644b7
lmnnarciso <lmnnarciso14@gmail.com>
2023-04-19 08:39:56 +0800

    making borrow checker angrier part 2

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,16 @@ fn add_one(mut item: &mut Item) {
     item.count += 1;
 }
 
-fn main() {
-    let mut item = Item { count: 1 };
+fn print_all(items: &Vec<Item>) {
+    for item in items {
+        println!("{:?}", item);
+    }
+}
 
-    println!("{:?}", item);
-    add_one(&mut item);
-    println!("{:?}", item);
+fn main() {
+    let mut items = vec![Item { count: 1 }];
+    let first = items.get_mut(0);
+    let second = items.get_mut(1);
+
+    println!("{:?}", second);
 }

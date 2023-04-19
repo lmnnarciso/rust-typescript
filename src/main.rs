575ecfa7fb90f359de540f5ec7cc6e9061d168c9
lmnnarciso <lmnnarciso14@gmail.com>
2023-04-19 08:02:28 +0800

    Error handling with Result enum

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,13 @@ fn main() {
         .expect("the filename to be passed in");
 
     let file = std::fs::read_to_string(arg).expect("unable to read the file to string");
+    let foo = Some(5);
 
-    file.lines().for_each(|line| println!("{}", line))
+    file.lines().for_each(|line| {
+        if let Ok(x) = line.parse::<usize>() {
+            println!("{}", x)
+        } else {
+            println!("Line not a number")
+        }
+    })
 }

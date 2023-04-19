fd319e0b2b879a2d2a9d6f8032035d85ce6c2205
lmnnarciso <lmnnarciso14@gmail.com>
2023-04-19 08:28:04 +0800

    making the borrow checker angry part 1

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
+#[derive(Debug)]
+struct Item {
+    count: usize,
+}
+
+fn add_one(mut item: &mut Item) {
+    item.count += 1;
+}
+
 fn main() {
-    let arg = std::env::args()
-        .nth(1)
-        .expect("the filename to be passed in");
+    let mut item = Item { count: 1 };
 
-    let file = std::fs::read_to_string(arg).expect("unable to read the file to string");
-    let foo = Some(5);
-
-    file.lines().for_each(|line| {
-        if let Ok(x) = line.parse::<usize>() {
-            println!("{}", x)
-        } else {
-            println!("Line not a number")
-        }
-    })
+    println!("{:?}", item);
+    add_one(&mut item);
+    println!("{:?}", item);
 }
